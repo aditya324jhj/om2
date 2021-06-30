be0b829e944b04745e3b0e82511d8797c54c01af
aditya324jhj <79002262+aditya324jhj@users.noreply.github.com>
2021-06-30 10:24:25 +0530

    Add files via upload

---
+++ letschat.js
@@ -0,0 +1,9 @@
+function addUser() {
+    user_name = document.getElementById("username").value;
+
+    localStorage.setItem("username" , user_name);
+
+    window.location = "letschat.html";
+}
+
+
